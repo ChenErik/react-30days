e8e92fb19d8b45798b3188d70bf55fe5d05ee09a
ChenErik <2392237608@qq.com>
2022-06-15 19:18:28 +0800

    feat:add tab scrollbar

--- 30-days-of-react/src/App.tsx
+++ 30-days-of-react/src/App.tsx
@@ -3,6 +3,7 @@ import TestOne from './components/testOne'
 import TestTwo from './components/testTwo'
 import TestThree from './components/testThree'
 import TestFour from './components/testFour'
+import TestFive from './components/TestFive'
 import type { TabItem } from './components/tab'
 import Tab from './components/tab'
 import './App.css'
@@ -22,18 +23,13 @@ function App() {
     setActive(item.value)
   }
   function getDayComponent(value: string | number): JSX.Element {
-    switch (value) {
-      case 1:
-        return <TestOne name="qeqzsad" skills={skillList} onClick={() => update({ id: 123 })}></TestOne>
-      case 2:
-        return <TestTwo num={32}></TestTwo>
-      case 3:
-        return <TestThree></TestThree>
-      case 4:
-        return <TestFour></TestFour>
-      default:
-        return <h1>没有对应的组件</h1>
-    }
+    const componentsMap = new Map<(number | string), JSX.Element>()
+    componentsMap.set(1, <TestOne name="qeqzsad" skills={skillList} onClick={() => update({ id: Math.floor(Math.random() * 100) })} />)
+    componentsMap.set(2, <TestTwo num={32} />)
+    componentsMap.set(3, <TestThree />)
+    componentsMap.set(4, <TestFour />)
+    componentsMap.set(5, <TestFive />)
+    return componentsMap.get(value) ?? <h1>没有对应的组件</h1>
   }
   const getData = async <T extends {}>(url: string): Promise<T> => {
     const response = await fetch(url)
---
+++ 30-days-of-react/src/components/TestFive.tsx
@@ -0,0 +1,8 @@
+const TestFive = () => {
+  return (
+        <div className="test-five">
+            <h1>this is test five</h1>
+        </div>
+  )
+}
+export default TestFive
