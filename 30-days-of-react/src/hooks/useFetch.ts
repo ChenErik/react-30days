098b7faf4dacde1189b849e538bd95e975b0ef30
ChenErik <2392237608@qq.com>
2022-06-16 18:42:28 +0800

    fix: useFetch

--- 30-days-of-react/src/hooks/useFetch.ts
+++ 30-days-of-react/src/hooks/useFetch.ts
@@ -1,21 +1,17 @@
 import { useEffect, useState } from 'react'
-import { getData } from '../utils'
+// import { getData } from '../utils'
 
 export const useFetch = <T>(url: string, defaultValue?: T): T => {
   const [data, setData] = useState<T>(defaultValue ?? [] as any)
   useEffect(() => {
     (async function load() {
-      try {
-        const data = await getData<T>(url)
+      const response = await fetch(url)
+      const data: T = await response.json()
+      if (response.status === 200)
         setData(data)
-      }
-      catch (err) {
-        // eslint-disable-next-line no-console
-        console.log(err)
+      else
         setData(defaultValue ?? [] as any)
-      }
     })()
   }, [])
   return data
 }
-
